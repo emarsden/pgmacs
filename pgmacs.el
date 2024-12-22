1ff244030b62649e76003617d1ad3d19ad2e646e
Eric Marsden <eric.marsden@risk-engineering.org>
2024-12-22 14:06:01 +0100

    Prepare for v0.19 release

--- pgmacs.el
+++ pgmacs.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2023-2024 Eric Marsden
 ;; Author: Eric Marsden <eric.marsden@risk-engineering.org>
-;; Version: 0.18
+;; Version: 0.19
 ;; Package-Requires: ((emacs "29.1") (pg "0.44"))
 ;; URL: https://github.com/emarsden/pgmacs/
 ;; Keywords: data, PostgreSQL, database
