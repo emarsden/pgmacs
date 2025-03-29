382439d2baef168a32c39c0ae47b05350e4ebfb8
Eric Marsden <eric.marsden@risk-engineering.org>
2025-03-29 13:50:30 +0100

    Update pg-el requirement to v0.51

--- pgmacs.el
+++ pgmacs.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2023-2025 Eric Marsden
 ;; Author: Eric Marsden <eric.marsden@risk-engineering.org>
 ;; Version: 0.23
-;; Package-Requires: ((emacs "29.1") (pg "0.50"))
+;; Package-Requires: ((emacs "29.1") (pg "0.51"))
 ;; URL: https://github.com/emarsden/pgmacs/
 ;; Keywords: data, PostgreSQL, database
 ;; SPDX-License-Identifier: GPL-3.0-or-later
