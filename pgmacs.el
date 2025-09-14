c30f082944c2bd48cbe06483026ac2b6963b8c2f
Eric Marsden <eric.marsden@risk-engineering.org>
2025-09-14 12:05:35 +0200

    Increase margin around comment icon

--- pgmacs.el
+++ pgmacs.el
@@ -274,7 +274,7 @@ e.g. `UTC' or `Europe/Berlin'. Nil for local OS timezone."
   (let* ((icon (svg-create "1em" "1em" :viewBox "0 0 32 32"))
          (d "M16 26c-1.168 0-2.296-.136-3.38-.367l-4.708 2.83.063-4.639C4.366 21.654 2 18.066 2 14 2 7.373 8.268 2 16 2s14 5.373 14 12c0 6.628-6.268 12-14 12Zm0-26C7.164 0 0 6.269 0 14c0 4.419 2.345 8.354 6 10.919V32l7.009-4.253c.97.16 1.968.253 2.991.253 8.836 0 16-6.268 16-14 0-7.731-7.164-14-16-14Zm7 11H9a1 1 0 0 0 0 2h14a1 1 0 1 0 0-2Zm-2 6H11a1 1 0 1 0 0 2h10a1 1 0 1 0 0-2Z"))
     (svg--append icon (dom-node 'path `((d . ,d) (fill . "#000") (fill-rule . "evenodd"))))
-    (svg-image icon :margin (cons 3 1) :ascent 'center)))
+    (svg-image icon :margin (cons 5 3) :ascent 'center)))
 
 (defun pgmacs--svg-icon-replication ()
   "Return an SVG icon representing data replication."
